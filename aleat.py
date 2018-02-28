8722497941586b89daf373c81f78cb1fe12d8314
apayol <apayolmontero@gmail.com>
2018-02-28 19:35:51 +0100

    Creado fichero aleat.py con el process de URLAleat

---
+++ aleat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import webapp
+import random
+
+class aleat(webapp.webApp):
+
+    def process(self, parsedRequest):
+        num_aleat = random.randint(1,9999999)
+        return ("200 OK", "<html><body><h1>Hola.</h1><p><a href= http://localhost:1234/"
+            + str(num_aleat) + ">Dame otra</a></p></body></html>")
+ 
+if __name__ == '__main__':
+    my_webapp = aleat("localhost",1234)
