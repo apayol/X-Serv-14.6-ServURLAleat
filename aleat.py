a4facf68fb551f388b4479e3adb64f6ce9caaa34
apayol <apayolmontero@gmail.com>
2018-02-28 19:47:23 +0100

    Corregidas normas de estilo

--- aleat.py
+++ aleat.py
@@ -8,7 +8,7 @@ class aleat(webapp.webApp):
 
     def process(self, parsedRequest):
         num_aleat = random.randint(1, 9999999)
-        return ("200 OK", "<html><body><h1>Hola.</h1>" + 
+        return ("200 OK", "<html><body><h1>Hola.</h1>" +
                 "<p><a href= http://localhost:1234/" + str(num_aleat) +
                 ">Dame otra</a></p></body></html>")
 
